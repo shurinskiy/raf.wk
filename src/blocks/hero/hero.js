5f0a9dbf88ef2f1fd9154c309b7ef162580a25ec
_tango <shurinskiy@mail.ru>
2021-07-24 18:10:04 +0600

    header & footer

--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -1,4 +1,14 @@
-(() => {
+import Swiper, { Pagination } from 'swiper/core';
+Swiper.use([Pagination]);
 
+(() => {
+	const swiper = new Swiper('.hero__slider', {
+		pagination: {
+			el: '.hero__slider-navi',
+			clickable: true,
+			bulletClass: 'hero__slider-dot',
+			bulletActiveClass: 'hero__slider-dot_active'
+		}
+	});
 
 })();
