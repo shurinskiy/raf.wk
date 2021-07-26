bcfed79291913cc5fd7a4e13cb789574d7de81b4
_tango <shurinskiy@mail.ru>
2021-07-26 08:16:04 +0600

    add slider navi

--- src/blocks/news-raf/news-raf.js
+++ src/blocks/news-raf/news-raf.js
@@ -5,6 +5,10 @@ Swiper.use([Navigation]);
 	const swiper = new Swiper('.news-raf__slider', {
 		slidesPerView: 4,
 		spaceBetween: 24,
+		navigation: {
+			nextEl: ".news-raf__btn-next",
+			disabledClass: "news-raf__btn-next_disabled"
+		}
 	});
 
 })();
