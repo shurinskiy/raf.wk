23a88d2dc14e229ebd49e5c4471a3c53a422e9f5
_tango <shurinskiy@mail.ru>
2021-07-26 18:21:26 +0600

    add gallery on mainpage

--- src/blocks/gm-slider/gm-slider.js
+++ src/blocks/gm-slider/gm-slider.js
@@ -1,4 +1,13 @@
+import Swiper, { Pagination } from 'swiper/core';
+Swiper.use([Pagination]);
+
 (() => {
-
-
+	const swiper = new Swiper('.gm-slider', {
+		pagination: {
+			el: '.gm-slider__navi',
+			clickable: true,
+			bulletClass: 'gm-slider__dot',
+			bulletActiveClass: 'gm-slider__dot_active'
+		}
+	});
 })();
