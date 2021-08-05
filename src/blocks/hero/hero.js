344ebc9ce3b1d2870152e904da231e049b2a147d
_tango <shurinskiy@mail.ru>
2021-08-05 21:20:09 +0600

    add several pages

--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -1,14 +1,25 @@
-import Swiper, { Pagination } from 'swiper/core';
-Swiper.use([Pagination]);
+import Swiper, { Pagination, Autoplay } from 'swiper/core';
+Swiper.use([Pagination, Autoplay]);
 
 (() => {
 	const swiper = new Swiper('.hero__slider', {
 		loop: true,
+		speed: 800,
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false
+		},
 		pagination: {
 			el: '.hero__slider-navi',
 			clickable: true,
 			bulletClass: 'hero__slider-dot',
-			bulletActiveClass: 'hero__slider-dot_active'
+			bulletActiveClass: 'hero__slider-dot_active',
+			bulletElement: 'span'
+		},
+		on: {
+			autoplay: function(e) {
+				// console.log(e.passedParams.autoplay.delay);
+			}
 		}
 	});
 
