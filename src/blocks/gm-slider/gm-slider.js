8bd01d2557a5e7fcc092ea1a29e1e07328c0c8a3
_tango <shurinskiy@mail.ru>
2021-08-06 12:15:26 +0600

    add slider progress-bar

--- src/blocks/gm-slider/gm-slider.js
+++ src/blocks/gm-slider/gm-slider.js
@@ -4,12 +4,24 @@ Swiper.use([Pagination]);
 (() => {
 	const swiper = new Swiper('.gm-slider', {
 		loop: true,
-
+		speed: 800,
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false
+		},
 		pagination: {
 			el: '.gm-slider__navi',
 			clickable: true,
 			bulletClass: 'gm-slider__dot',
-			bulletActiveClass: 'gm-slider__dot_active'
+			bulletActiveClass: 'gm-slider__dot_active',
+			renderBullet: function (index, className) {
+				let svg = '';
+
+				if(!! this.passedParams.autoplay) {
+					svg = `<svg height="20" width="20" style="animation-duration:${this.passedParams.autoplay.delay/1000}s;"><circle class="circle" cx="10" cy="10" r="9"/></svg>`;
+				}
+				return `<span class="${className}">${svg}</span>`;
+			}
 		}
 	});
 })();
--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -14,13 +14,14 @@ Swiper.use([Pagination, Autoplay]);
 			clickable: true,
 			bulletClass: 'hero__slider-dot',
 			bulletActiveClass: 'hero__slider-dot_active',
-			bulletElement: 'span'
-		},
-		on: {
-			autoplay: function(e) {
-				// console.log(e.passedParams.autoplay.delay);
+			renderBullet: function (index, className) {
+				let svg = '';
+
+				if(!! this.passedParams.autoplay) {
+					svg = `<svg height="20" width="20" style="animation-duration:${this.passedParams.autoplay.delay/1000}s;"><circle class="circle" cx="10" cy="10" r="9"/></svg>`;
+				}
+				return `<span class="${className}">${svg}</span>`;
 			}
 		}
 	});
-
 })();
